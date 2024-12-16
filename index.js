29712564544d5d0c6409fe077ef806ed1189f427
Hossein samiei <syhsamiei@gmail.com>
2024-12-16 13:34:36 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+const COLORS = ["red", "green", "blue", "yellow", "purple", "orange"];
+
+function findColor(color) {
+
+}
+module.exports = findColor;
