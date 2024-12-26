aa2b6a2dbcc1b56209d1170b87c6a4165ff32bbd
Hossein <syhsamiei@gmail.com>
2024-12-26 15:38:10 +0330

    fix

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 const COLORS = ["red", "green", "blue", "yellow", "purple", "orange"];
 
 function findColor(color) {
-
+    const bytes = utf8ToBytes(color);
+    const hash = sha256(bytes);
+    const hex = toHex(hash);
+    return COLORS.find((c) => toHex(sha256(utf8ToBytes(c))) === hex);
 }
 module.exports = findColor;
+
+// what is rainbow table? 
+// rainbow table is a precomputed table of hashes that can be used to crack passwords. 
+
+const a = "apple";
+const abytes = utf8ToBytes(a);
+const hash = sha256(abytes);
+console.log(toHex(hash));
+
+const b = "banana";
+const bbytes = utf8ToBytes(b);
+const bhash = sha256(bbytes);
+console.log(toHex(bhash));
+
+
