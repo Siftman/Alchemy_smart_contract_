e30ab0bd3995439357795923f842f2de961f8053
siftman <ssiftman@gmail.com>
2024-12-26 19:08:16 +0330

    feat: add sign function and recover pk function

--- hashMessage.js
+++ hashMessage.js
@@ -1,6 +1,9 @@
 const { keccak256 } = require("ethereum-cryptography/keccak");
 const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
+const { secp256k1 } = require("ethereum-cryptography/secp256k1");
 
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
 
 function hashMessage(message) {
     const bytes = utf8ToBytes(message);
@@ -8,4 +11,13 @@ function hashMessage(message) {
     return toHex(hash);
 }
 
-module.exports = hashMessage('Vote on proposal num 325');
+async function signMessage(msg) {
+    let hashed_msg = hashMessage(msg);
+    return secp256k1.sign(hashed_msg, PRIVATE_KEY, { recoverKey: true });
+}
+
+async function recoverKey(message, signature, recoveryBit) {
+    let result = secp256k1.recoverPublicKey(hashMessage(message), signature, recoveryBit);
+    console.log(result);
+}
+recoverKey('TRX:2025-on-chain', signMessage('TRX:2025-on-chain'), 1);
